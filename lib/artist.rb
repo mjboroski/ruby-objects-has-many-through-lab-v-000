e6a6d14b2961dd686117318dbb44b7ac07de4e4d
Michael Boroski <mjboroski@gmail.com>
2018-04-26 04:52:03 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,23 @@
 class Artist
 
+  attr_accessor :name, :songs, :genres
+
+  def innitialize(name)
+    @name=name
+    @songs=[]
+  end
+
+  def add_song(song)
+    @songs<<song
+    song.artist<<self
+  end
+
+  def songs
+    @songs
+  end
+
+  def genres
+    @songs.each do {|song| @genres<<song.genre}
+  end
 
 end
