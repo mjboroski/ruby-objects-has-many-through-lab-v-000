1ddbb0243940fc8e75fc3c925859ea727b69a9df
Michael Boroski <mjboroski@gmail.com>
2018-04-26 05:00:51 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,5 +1,23 @@
 class Doctor
 
-  attr_accessor
+  attr_accessor :name, :songs, :genres
+
+  def innitialize(name)
+    @name=name
+    @songs=[]
+  end
+
+  def add_song(song)
+    @songs<<song
+    song.artist<<self
+  end
+
+  def songs
+    @songs
+  end
+
+  def genres
+    @songs.each do {|song| @genres<<song.genre}
+  end
 
 end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,23 @@
 class Patient
 
+  attr_accessor :songs, :artists, :name
+
+  def initialize(name)
+    @name=name
+    @songs=[]
+  end
+
+  def add_song(song)
+    @songs<<song
+  end
+
+  def songs
+    @songs
+  end
+
+  def artists
+    @songs.each do |song|
+    @artists<<song.artist
+  end
 
 end
